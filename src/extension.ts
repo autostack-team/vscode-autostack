e4e808ccc4d6fef7f481d13b3b1a96837d146ecb
Elijah Sawyers <elijahsawyers@gmail.com>
2019-11-15 14:15:36 -0600

    Added logo

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,24 @@
  * Author: Elijah Sawyers
  * Date: 11/15/2019
  */
-import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {}
+import * as vscode from 'vscode';
+import * as path from 'path';
+
+export function activate(context: vscode.ExtensionContext) {
+    vscode.commands.registerCommand('autostack', () => {
+        const panel = vscode.window.createWebviewPanel(
+          'autostack',
+          'autostack',
+          vscode.ViewColumn.Beside,
+          {}
+        );
+
+        panel.iconPath = {
+          light: vscode.Uri.file(path.join(context.extensionPath, 'assets', 'icon.png')),
+          dark: vscode.Uri.file(path.join(context.extensionPath, 'assets', 'icon.png'))
+        }
+      });
+}
 
 export function deactivate() {}
